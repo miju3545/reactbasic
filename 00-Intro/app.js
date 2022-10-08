8e77855151d646c0843ef26d3922aa06e8e3fc73
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-08 10:17:16 +0900

    [Modified] 00-Intro

--- 00-Intro/app.js
+++ 00-Intro/app.js
@@ -1,4 +1,27 @@
-function createElement(type, props) {
+function createH1(props) {
+	return [document.createElement('h1')]
+		.map((element) => Object.entries({ ...props, 'data-id': 'subject' }))
+		.forEach(([name, value]) => element.setAttribute(name, value));
+}
+function createDiv(props) {
+	return [document.createElement('h1')]
+		.map((element) => Object.entries({ ...props, 'data-id': 'layout' }))
+		.forEach(([name, value]) => element.setAttribute(name, value));
+}
+
+const createMap = {
+	h1: createH1,
+	div: createDiv,
+};
+
+const coupler = (map) => (type, props) => map[type](props);
+
+const createElement = coupler(createMap);
+
+const myProps = {className: "testZone"}
+console.log(createElement(myProps);
+
+function createElement2(type, props) {
 	return [document.createElement(type)]
 		.map((element) => Object.entries({ ...props, 'data-id': 'title' }))
 		.forEach(([name, value]) => element.setAttribute(name, value))[0];
--- 00-Intro/index.js
+++
@@ -1,5 +0,0 @@
-function createElement(type, props) {
-	return [document.createElement(type)]
-		.map((element) => Object.entries({ ...props, 'data-id': 'title' }))
-		.forEach(([name, value]) => element.setAttribute(name, value))[0];
-}
